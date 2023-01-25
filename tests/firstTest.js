fd4c6fc9037df68d13ef72c8fe33d5de72f14462
Sunil Yadiki <sunil.yadiki@MACPRO404.local>
2023-01-25 19:34:51 +0530

    first commit

---
+++ tests/firstTest.js
@@ -0,0 +1,23 @@
+const {Builder, By, Button, Key} = require ("selenium-webdriver");
+const assert = require("assert")
+
+async function example(){
+
+// launch the browser
+let driver = await new Builder().forBrowser("firefox").build();
+//navigate to our application
+await driver.get("https://www.juniper.net/");
+//add
+await driver.findElement(By.id("nav1")).click();
+await driver.findElement(By.xpath("// a [@class = 'link-animation'][contains(text(), 'IT Networking')]")).click();
+await driver.findElement(By.xpath("//div[@class='subnav__wrapper']//a[contains(text(), 'IT Networking')]")).click();
+
+let todoText = await driver.findElement(By.xpath("(//span[contains(text(),'IT Networking')])[1]")).getText().then(function(value){
+    return value
+});
+assert.strictEqual(todoText, "IT Networking");
+//close the browser
+await driver.quit();
+
+}
+example()
